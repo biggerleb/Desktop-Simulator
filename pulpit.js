abb01d5cb159e771c04af6e1c82f53b4bb79fa7b
Marek Mucha <marek.mucha99@gmail.com>
2018-11-05 22:26:59 +0100

    updating css grid on resize

--- pulpit.js
+++ pulpit.js
@@ -7,7 +7,27 @@ $(document).ready(function(){
 function updatingGrid(){
 	var windowHeight = window.innerHeight;
 	var windowWidth = window.innerWidth;
-	
+	var xItemsNum = Math.floor(windowWidth / 100);
+	var yItemsNum = Math.floor(windowHeight / 100);
+	var itemsCapacity = xItemsNum * yItemsNum;
+	var itemsAlreadyCreated = getNumberOfItems();
+	var items = $(".item");
+	// when screen gets smaller  !!!ERROR WHEN MORE FULL ITEMS THAN THERE IS SPACE IN NEW GRID - solution get those files to new folder
+	if(items.length > itemsCapacity){
+		var counter = 0;
+		var index = items.length - 1; 
+		while(counter <= items.length - itemsCapacity - 1) {
+			if(!($(items[index]).hasClass("full"))){
+				$(items[index]).remove();
+				counter++;
+				index--;
+			}
+		}
+	}
+	// when screen gets bigger
+	if(items.length < itemsCapacity){
+		fillingGrid();
+	}
 }
 
 function fillingGrid(){
